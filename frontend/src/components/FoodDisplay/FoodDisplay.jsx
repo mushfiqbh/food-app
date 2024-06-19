81c89402a188643a8270710b5756b81583c6cedb
mushfiqbh <mushfiqbh@gmail.com>
2024-06-19 20:17:26 +0600

    Frontend Completed

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,32 @@
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
+import "./FoodDisplay.css";
+import React, { useContext } from "react";
+
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if ((category === "All" || category === item.category)) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                price={item.price}
+                desc={item.description}
+                image={item.image}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FoodDisplay;
---
+++ frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./LoginPopUp.css";
+import { assets } from "../../assets/assets";
+
+const LoginPopUp = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Sign Up");
+
+  return (
+    <div className="login-popup">
+      <form className="login-popup-dialogue">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your Name" required />
+          )}
+
+          <input type="email" placeholder="Your Email" required />
+          <input type="password" placeholder="Your Password" required />
+        </div>
+        <button>
+          {currentState === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>Agree to the terms and contdion</p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrentState("Sign Up")}>Click Here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrentState("Login")}>Login Here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopUp;
