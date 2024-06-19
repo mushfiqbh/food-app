1679e11959d3c36b9ccdd646cd7b076545cf82b1
mushfiqbh <mushfiqbh@gmail.com>
2024-06-19 22:16:08 +0600

    Frontend Update

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -7,7 +7,7 @@ import { food_list } from "../../assets/assets";
 const Cart = () => {
   const { cartItems, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -23,29 +23,24 @@ const Cart = () => {
         <br />
         <hr />
 
-        {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0) {
-            return (
-              <div>
-                <div key={index} className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
-                  <p>{item.name}</p>
-                  <p>${item.price}</p>
-                  <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
-                  <p
-                    onClick={() => removeFromCart(cartItems[item._id])}
-                    className="cross"
-                  >
-                    X
-                  </p>
-                </div>
-                <hr />
-              </div>
-            );
-          }
-        })}
+        {food_list.map((item) => (
+          <div key={item._id} className="cart-items-title cart-items-item">
+            {cartItems[item._id] > 0 && (
+              <>
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${item.price * cartItems[item._id]}</p>
+                <p onClick={() => removeFromCart(item._id)} className="cross">
+                  X
+                </p>
+              </>
+            )}
+          </div>
+        ))}
       </div>
+
       <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Total</h2>
